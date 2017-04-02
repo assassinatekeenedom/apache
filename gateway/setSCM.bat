8d76c08e774fa8ed5b346d880bbf571c706baac4
Nathan Keene <robert.keene@optum.com>
2017-04-01 20:17:03 -0400

    setSCM.bat no longer throws errors; found out I could use a simpler if statement

--- gateway/setSCM.bat
+++ gateway/setSCM.bat
@@ -2,29 +2,21 @@
 setlocal
 set /p git="What is the URL for the remote git repository (press enter for none)? "
 if "%git%" == "" (
-	if exist ".scm-git" (
-		del .scm-git
-	)
+	if exist ".scm-git" del .scm-git
 ) else (
 	echo %git%>.scm-git
 )
 set /p svnTrunk="What is the URL for the svn Trunk repository (press enter for none)? "
 if "%svnTrunk%" == "" (
-	if exist ".scm-svn-trunk" (
-		del .scm-svn-trunk
-	)
-	if exist ".scm-svn-branch" (
-		del .scm-svn-branch
-	)
+	if exist ".scm-svn-trunk" del .scm-svn-trunk
+	if exist ".scm-svn-branch" del .scm-svn-branch
 	goto:donescm
 ) else (
 	echo %svnTrunk%>.scm-svn-trunk
 )
 set /p svnBranch="What is the base URL for branches from %svnTrunk%? "
 if "%svnBranch%" == "" (
-	if exist ".scm-svn-branch" (
-		del .scm-svn-branch
-	)
+	if exist ".scm-svn-branch" del .scm-svn-branch
 ) else (
 	echo %svnBranch%>.scm-svn-branch
 )
