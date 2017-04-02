012da0e701b36b7208424083fda78a9c123cb71f
Nathan Keene <robert.keene@optum.com>
2017-04-01 20:25:20 -0400

    slightly improved setSCM.bat for use in creating and checking out branches; and name simplification

--- gateway/setSCM.bat
+++ gateway/setSCM.bat
@@ -6,14 +6,15 @@ if "%git%" == "" (
 ) else (
 	echo %git%>.scm-git
 )
-set /p svnTrunk="What is the URL for the svn Trunk repository (press enter for none)? "
+set /p svnTrunk="What is the base URL for svn Trunk repositories (press enter for none)? "
 if "%svnTrunk%" == "" (
 	if exist ".scm-svn-trunk" del .scm-svn-trunk
 	if exist ".scm-svn-branch" del .scm-svn-branch
 	goto:donescm
 ) else (
-	echo %svnTrunk%>.scm-svn-trunk
+	goto:trunkName
 )
+:branchq
 set /p svnBranch="What is the base URL for branches from %svnTrunk%? "
 if "%svnBranch%" == "" (
 	if exist ".scm-svn-branch" del .scm-svn-branch
@@ -23,3 +24,13 @@ if "%svnBranch%" == "" (
 :donescm
 exit /b 0
 endlocal
+
+:trunkName
+set /p svnName="What is the name of the target repository in Trunk? "
+if "%svnName%" == "" (
+	goto:trunkName
+) else (
+	echo %svnName%>.named
+)
+echo %svnTrunk%/%svnName%>.scm-svn-trunk
+goto:branchq
