93940ced933f76da44f319484160ba6fbf14737c
Nathan Keene <robert.keene@optum.com>
2017-04-02 01:00:18 -0400

    verified that creation of new branches is working properly (integrated git/svn)

--- gateway/.branch.bat
+++ gateway/.branch.bat
@@ -16,24 +16,13 @@ if exist "%branch%" (
 )
 :scmq
 if exist "..\.scm-git" (
-	cd ..
-	set /p scm=<.scm-git
-	cd .branch
-	git clone %scm% git-%branch%
-	cd git-%branch%
-	git checkout -b %branch%
-	git push origin %branch%
-	cd ..
+	goto:gitcreateq
 )
+:scmsvnq
 if exist "..\.scm-svn-trunk" (
-	cd ..
-	set /p scm=<.scm-svn-trunk
-	set /p branch=<.scm-svn-branch
-	set /p named=<.named
-	cd .branch
-	svn copy %scm% %branch%/%named%-%branch% -m "%branch% - initializing branch repository.">nul
-	svn co %branch%/%named%-%branch% %branch%>nul
+	goto:svncreateq
 )
+:scmcheckq
 if "%scm%" == "" (
 	call setSCM.bat
 	goto:scmq
@@ -45,15 +34,35 @@ if exist "git-%branch%" (
 		xcopy .gitignore ..\%branch%\.gitignore*>nul
 		cd ..
 		rmdir /s /q git-%branch%
-		goto:doneqb
+	) else (
+		xcopy git-%branch% %branch%\ /s /i /h /q>nul
+		rmdir /s /q git-%branch%
 	)
-	xcopy git-%branch% %branch%\ /s /i /h /q>nul
-	rmdir /s /q git-%branch%
-	goto:doneqb
 )
-:doneqb
 if not exist "%branch%" (
 	echo There was an ERROR of some kind; no Branch named %branch% created!!
 )
 cd ..
 endlocal
+exit /b 0
+
+:gitcreateq
+cd ..
+set /p scm=<.scm-git
+cd .branch
+git clone %scm% git-%branch%>nul
+cd git-%branch%
+git checkout -b %branch%>nul
+git push origin %branch%>nul
+cd ..
+goto:scmsvnq
+
+:svncreateq
+cd ..
+set /p scm=<.scm-svn-trunk
+set /p scmbranch=<.scm-svn-branch
+set /p named=<.named
+cd .branch
+svn copy %scm% %scmbranch%/%named%-%branch% -m "%branch% - initializing branch repository.">nul
+svn co %scmbranch%/%named%-%branch% %branch%>nul
+goto:scmcheckq
