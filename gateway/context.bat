ac215d3032221b93e248e930dd39a74d22477898
Nathan Keene <robert.keene@optum.com>
2017-04-01 21:21:00 -0400

    making progress cleaning context.bat; pretty much acceptable now

--- gateway/context.bat
+++ gateway/context.bat
@@ -1,33 +1,27 @@
 @ECHO OFF
 setlocal
-if not exist ".repo" (
-	echo the .repo needs to be set; but not necessarily .scm-svn or .scm-git.
-	call setRepo.bat
-)
-echo context: %1
-echo repo-based-path: %2
-echo proxy port: %3
-if exist "%module%" (
-	cd %module%
-)
-:skiptestsq
-set /p skipTests="Do you want to skip tests [Y/N]? " 
-if /i "%skipTests%" == "Y" (
-	echo mvn clean install -DskipTests>.build.bat
-	call .build.bat
-	del .build.bat
+if "%1%" == "" (
+	goto:manualcq
 ) else (
-	if /i "%skipTests%" == "N" (
-		echo mvn clean org.jacoco:jacoco-maven-plugin:0.7.2.201409121644:prepare-agent install>.build.bat
-		call .build.bat
-		del .build.bat
-	) else (
-		goto:skiptestsq
-	)
+	echo %1 %2 %3>>.static-contexts
+	goto:finishq
 )
+:manualcq
+set /p context="What is the URL context you want to intercept? " 
+if "%context%" == "" (
+	goto:manualcq
+)
+:routeq
+set /p route="Where (relative to the repo root) should this intercept route to (folder path)? " 
+if "%route%" == "" (
+	goto:routeq
+)
+:portq
+set /p port="What port will you use for this intercept routing? "
+if "%port%" == "" (
+	goto:portq
+)
+echo %context% %route% %port%>>.static-contexts
+:finishq
 endlocal
 exit /b 0
-echo /odxp/js/ %repo%\src\odxp_ui\src\main\webapp\js 1337>.static-contexts
-echo /odxp/templates/ %repo%\src\odxp_ui\src\main\webapp\templates 1338>>.static-contexts
-echo /odxp/css/ %repo%\src\odxp_ui\src\main\webapp\css 1339>>.static-contexts
-echo /odxp/uitk-lib-custom/ %repo%\src\odxp_ui\src\main\webapp\uitk-lib-custom 1340>>.static-contexts
