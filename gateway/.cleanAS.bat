dbb7aedfb18fb6a956051a7a2bbbb47d49f042ea
Nathan Keene <robert.keene@optum.com>
2017-04-02 23:04:50 -0400

    Things are working smoothly; except for the java code that won't compile

--- gateway/.cleanAS.bat
+++ gateway/.cleanAS.bat
@@ -11,19 +11,24 @@ if not "%1%"=="" (
 	del %1
 	goto:cleanfinishq
 )
+:cleanmoreq
 dir /d
-:cleanpatternq
-set /p clean="What file pattern do you want to delete? "
-if "%clean%" == "" goto:cleanpatternq
-del %clean%
 set /p more="Do you have more files to delete? "
 if /i "%more%" == "Y" (
 	goto:cleanpatternq
 ) else (
 	if /i not "%more%" == "N" ( 		
-		goto:cleanpatternq
+		goto:cleanmoreq
 	)
+	goto:cleanfinishq
 )
+:cleanpatternq
+set /p clean="What file pattern do you want to delete? "
+if "%clean%" == "" goto:cleanmoreq
+echo deleting "%clean%" at "%cd%"
+del %clean%
+goto:cleanmoreq
+
 :cleanfinishq
 cd %odxp%
 endlocal
--- gateway/build.bat
+++ gateway/build.bat
@@ -1,57 +1,54 @@
 @ECHO OFF
 setlocal
-set /p domain=<.domain
+:buildrepoq
+if not exist ".repo" (
+	call setRepo.bat
+	goto:buildrepoq
+)
 set /p repo=<.repo
 set odxp=%cd%
-cd %repo%
-git fetch -avp
-cd %odxp%
-set /p userstory="What is the [USXXXXXX] (User Story Rally Id) do you want to build? "
-echo Domain: %domain%
-echo Repository Location: %repo%
-cd %repo%
-git checkout %userstory%
-git pull
-git status
+call .cleanAS.bat
 :testappq
-set /p testappbuild="Do you want to build the login web-application, context=/odxploginapp/ [Y/N]? "
-if /i "%testappbuild%" == "Y" (
-	cd testapp
-	goto:skiptestsq
+cd %repo%
+dir /d
+set /p build="Do you want to build an application [Y/N]? "
+if /i "%build%" == "Y" (
+	goto:deployappq
 ) else (
-	if /i "%testappbuild%" == "N" ( 
-		goto:moduleq
+	if /i "%build%" == "N" ( 
+		goto:buildfinishq
 	)
 	goto:testappq
 )
-:moduleq
-cd src
-dir
-set /p module="What module do you want to build? "
-if exist "%module%" (
+:deployappq
+set /p module="Provide the relative (from repo) path to the root of the application you want to deploy (leave empty if done): "
+if not "%module%" == "" (
+	if not exist "%repo%\%module%" echo could not find the application. && goto:deployappq
 	cd %module%
-)
+	goto:skiptestsq
+) 
+goto:testappq
+
+
 :skiptestsq
 set /p skipTests="Do you want to skip tests [Y/N]? " 
 if /i "%skipTests%" == "Y" (
 	echo mvn clean install -DskipTests>.build.bat
 	call .build.bat
-	del .build.bat
-) else (
-	if /i "%skipTests%" == "N" (
-		echo mvn clean org.jacoco:jacoco-maven-plugin:0.7.2.201409121644:prepare-agent install>.build.bat
-		call .build.bat
-		del .build.bat
-	) else (
-		goto:skiptestsq
-	)
-)
-cd %repo%
-set /p another="Do you want to do another build [Y/N]? " 
-if /i "%another%" == "Y" (
+	if exist ".build.bat" del .build.bat
+	goto:testappq
+) 
+if /i "%skipTests%" == "N" (
+	echo mvn clean org.jacoco:jacoco-maven-plugin:0.7.2.201409121644:prepare-agent install>.build.bat
+	call .build.bat
+	if exist ".build.bat" del .build.bat
 	goto:testappq
 )
-cd %odxp%
+goto:skiptestsq
+
+:buildfinishq
 call .cleanAS.bat
+call .deploy.bat
+cd %odxp%
 endlocal
 exit /b 0
