e19a328c917a8cd32505732f4e16c8d5a021b03c
Nathan Keene <robert.keene@optum.com>
2017-06-09 08:01:42 -0400

    more fixes to scripts.

--- gateway/theme.bat
+++ gateway/theme.bat
@@ -1,13 +1,17 @@
 @ECHO OFF
 setlocal
 if not "%1%"=="" (
-	set theme=%1
+	set theme=%1%
 	goto:theme
 )
 :retry
 set /p theme="What is the [name].bat of the theme (e.g. [cloud|error|build|scripts|server|snow][Invert])? "
 :theme
 call skin\context\%theme%.bat
+if not "%1%" == "" ( 
+	goto:qtheme 
+)
 set /p retry="Would you like to start over [Y/N]? "
 if /i "%retry%" == "Y" goto:retry
+:qtheme
 endlocal
